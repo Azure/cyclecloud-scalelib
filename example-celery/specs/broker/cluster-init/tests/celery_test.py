83471285e3bdd32a747a9fdd18f2900b496cd2cf
James Bell <jamesbell@microsoft.com>
2021-03-29 16:04:17 -0700

    Standardized cluster-init scripts, project.ini, and fixed 'None' error in celery autoscale script

---
+++ example-celery/specs/broker/cluster-init/tests/celery_test.py
@@ -0,0 +1,2 @@
+def test_add_task():
+    pass
--- example-celery/specs/worker/cluster-init/scripts/02.start.worker.sh
+++ example-celery/specs/worker/cluster-init/scripts/02.start.worker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+INSTALLDIR=/opt/cycle/scalelib
 BROKER_IP=$(jetpack config celery.broker.privateip localhost)
 sed  "s/__localhost__/$BROKER_IP/g" $CYCLECLOUD_SPEC_PATH/files/app.py > /root/tasks.py  
-source /root/.venv/celery/bin/activate
+source $INSTALLDIR/venv/bin/activate
 
 pushd /root
 celery -A tasks -O fair worker --loglevel=info 2>&1 > /var/log/celery.worker.log &
