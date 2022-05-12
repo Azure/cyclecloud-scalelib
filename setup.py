25f143b12633071181dffedae3856c232c3e5deb
Ryan Hamel <ryan.hamel@microsoft.com>
2022-05-12 12:35:26 -0400

    WIP

--- setup.py
+++ setup.py
@@ -274,8 +274,8 @@ setup(
     package_dir={"": "src", "conf": "conf"},
     include_package_data=True,
     install_requires=[
-        "requests == 2.24.0",
-        "typing_extensions",
+        "requests >= 2.24.0",
+        "typing_extensions==3.7.4.3",
         "immutabledict==1.0.0",
         "jsonpickle==1.4.1",
         "argcomplete==1.12.2",
